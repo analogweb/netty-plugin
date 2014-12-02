fb264044f1c5399dc51a62040ab07d8581608151
snowgooseyk <snowgoose.yk@gmail.com>
2014-12-03 01:35:45 +0900

    Add test case.

---
+++ src/test/java/org/analogweb/netty/IntegrationTest.java
@@ -0,0 +1,91 @@
+package org.analogweb.netty;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.analogweb.Server;
+import org.analogweb.annotation.Get;
+import org.analogweb.annotation.Route;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+
+/**
+ * @author snowgooseyk
+ */
+@Route("/")
+public class IntegrationTest {
+
+    private static Server server;
+
+    @Route
+    @Get
+    public String hello() {
+        return "Hello, World.";
+    }
+
+    @Route
+    @Get
+    public String exception() {
+        throw new IllegalStateException();
+    }
+
+    @BeforeClass
+    public static void oneTimeSetUp() throws Exception {
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        es.submit(new Runnable() {
+
+            @Override
+            public void run() {
+                server = HttpServers.create("http://localhost:18765/");
+                server.run();
+            }
+        });
+        Thread.sleep(2000);
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() {
+        server.shutdown(0);
+    }
+
+    @Test
+    public void testOk() throws Exception {
+        AsyncHttpClient cli = new AsyncHttpClient();
+        try {
+            Response r = cli.prepareGet("http://localhost:18765/hello").execute().get();
+            assertThat(r.getStatusCode(), is(200));
+            assertThat(r.getResponseBody(), is("Hello, World."));
+        } finally {
+            cli.close();
+        }
+    }
+
+    @Test
+    public void testNotFound() throws Exception {
+        AsyncHttpClient cli = new AsyncHttpClient();
+        try {
+            Response r = cli.prepareGet("http://localhost:18765/notfound").execute().get();
+            assertThat(r.getStatusCode(), is(404));
+        } finally {
+            cli.close();
+        }
+    }
+
+    @Test
+    public void testServerError() throws Exception {
+        AsyncHttpClient cli = new AsyncHttpClient();
+        try {
+            Response r = cli.prepareGet("http://localhost:18765/exception").execute().get();
+            assertThat(r.getStatusCode(), is(500));
+        } finally {
+            cli.close();
+        }
+    }
+}
